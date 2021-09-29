dc8aecb7ff6a4a905e3294834438ee031c8d197b
luiscaroortiz <luiscaroortiz@gmail.com>
2021-09-28 19:00:00 -0500

    se corrigen errores en las clases API de proveedores y ventas

--- src/main/java/Mintictest/ciclo3/api/proveedoresAPI.java
+++ src/main/java/Mintictest/ciclo3/api/proveedoresAPI.java
@@ -1,6 +1,7 @@
 package Mintictest.ciclo3.api;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +11,13 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import Mintictest.ciclo3.dao.proveedoresDAO;
 import Mintictest.ciclo3.model.proveedores;
 
 
 @RestController
-@RequestMapping ("ventas")
+@RequestMapping ("proveedores")
 public class proveedoresAPI {
 	
 	@Autowired
@@ -37,7 +39,7 @@ public class proveedoresAPI {
 	public void eliminar (@PathVariable ("id") Integer id) {
 		proveedoresDAO.deleteById(id);
 	}
-
+	
 	@PutMapping ("/actualizar")
 	public void actualizar (@RequestBody proveedores proveedores) {
 		proveedoresDAO.save(proveedores);
--- src/main/java/Mintictest/ciclo3/api/ventasAPI.java
+++ src/main/java/Mintictest/ciclo3/api/ventasAPI.java
@@ -1,6 +1,7 @@
 package Mintictest.ciclo3.api;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +11,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import Mintictest.ciclo3.dao.ventasDAO;
 import Mintictest.ciclo3.model.ventas;
+
 
 @RestController
 @RequestMapping ("ventas")
@@ -22,22 +25,25 @@ public class ventasAPI {
 	
 	@PostMapping ("/guardar")
 	public void guardar(@RequestBody ventas ventas) {
-		ventasDAO.save(ventas);	
+		ventasDAO.save(ventas);
+		
 	}
 	
 	@GetMapping("/listar")
 	public List <ventas> listar(){
-		return ventasDAO.findAll();	
+		return ventasDAO.findAll();
+		
 	}
 	
 	@DeleteMapping ("/eliminar/{id}")
 	public void eliminar (@PathVariable ("id") Integer id) {
 		ventasDAO.deleteById(id);
 	}
-
+	
 	@PutMapping ("/actualizar")
 	public void actualizar (@RequestBody ventas ventas) {
 		ventasDAO.save(ventas);
 	}
+	
 
 }
