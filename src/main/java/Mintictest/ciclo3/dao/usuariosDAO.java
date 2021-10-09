301b4c037a9f2778df925f6da70c2a4e8f4ddffa
adrianprieto85 <adrian@192.168.0.11>
2021-10-08 19:58:12 -0500

    se agrego comentario

--- src/main/java/Mintictest/ciclo3/dao/usuariosDAO.java
+++ src/main/java/Mintictest/ciclo3/dao/usuariosDAO.java
@@ -3,7 +3,7 @@ package Mintictest.ciclo3.dao;
 
 import Mintictest.ciclo3.model.usuarios;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//modificacion de integer a long
 public interface usuariosDAO extends JpaRepository<usuarios, Long>{
 	
 }
